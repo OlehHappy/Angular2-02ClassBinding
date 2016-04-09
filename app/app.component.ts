1eb3ea6f37fa22ab92612ecaa295e7f30f560052
Admin <olegtranslator@gmail.com>
2016-04-09 23:40:50 +0200

    class binding

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <button class="btn btn-primary" [class.active]="isActive">Submit</button>
+    `
+})
+export class AppComponent {
+    isActive = true;
+}
